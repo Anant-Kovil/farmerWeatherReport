2c2ce8b68aec7116e86b10d9e93bf2b5b55d2747
Anant-Kovil <75954126+Anant-Kovil@users.noreply.github.com>
2024-04-17 17:04:06 -0400

    added changes to UI and application

--- app.py
+++ app.py
@@ -12,23 +12,34 @@ from streamlit_folium import st_folium
 import folium
 load_dotenv()
 
-st.title("AI Powered Weather Forecasting App for Farmers")
+st.image('Kissan Dost Logo.png')
+st.title("Kisaan Dost")
 
 brazil = os.getenv('brazil')
 iowa = os.getenv('iowa')
 
 option = st.selectbox(
    "Which Farm Location?",
-   ("Brazil", "Iowa", "Custom"),
+   ("Taquara Farm in Rio de Janeiro - Brazil", "Susan's Sweet Potatoes Farm - Iowa", "Custom"),
    index=None,
    placeholder="Select location...",
 )
 
+optionCrop = st.selectbox(
+   "What type of crop is being produced?",
+   ("Potatoes", "Corn", "Sweet Potatos", "Carrots", "Watermelon", "Custom"),
+   index=None,
+   placeholder="Select Crop...",
+)
+
+if (optionCrop == "Custom"):
+  custom_crop = st.text_input("Crop", value="")
+
 st.write('You selected:', option)
 
 if (option == "Custom"):
-  DEFAULT_LATITUDE = 51.
-  DEFAULT_LONGITUDE = 3.
+  DEFAULT_LATITUDE = 42.35
+  DEFAULT_LONGITUDE = -71.08
 
   m = folium.Map(location=[DEFAULT_LATITUDE, DEFAULT_LONGITUDE], zoom_start=10)
 
@@ -80,17 +91,21 @@ def main(api_url):
                 f"humidity of {avg_humidity:.2f}%.")
       reports.append(report)
   
-  final_report = " ".join(reports)
+  final_report_unmerged = " ".join(reports)
+  final_report_weather = "The weather forecast for the next 5 days is as follows:\n\n" + final_report_unmerged + "\n\n"
+  final_report_soil = "The soil conditions are as follows:\n\n" + "The upcoming week's soil conditions are expected to undergo significant changes due to varied weather patterns. With several days of rain forecasted, the soil will likely retain higher moisture levels, which can be beneficial for water-dependent crops but may pose challenges for drainage-sensitive ones. The consistent rainfall until April 20 suggests that the soil will be highly saturated, increasing the risk of soil erosion and nutrient leaching, especially on slopes and in poorly drained areas. However, the break in precipitation by April 21 and 22 could allow the top layers of soil to dry out slightly, reducing the risk of anaerobic conditions which harm root growth. This period of no rain, combined with relatively stable temperatures, may offer an ideal window for planting or applying soil amendments, as the conditions allow for easier working of the soil and better root establishment." + "\n\n"
+  final_report_market_conditions = "Produce conditions are as follows:\n\n" + "Prices are experiencing moderate fluctuations due to an increased global demand for biofuels and animal feed, which corn is a primary component of. However, farmers are facing cost pressures from rising prices for fertilizers and pesticides, which are crucial for maintaining high yields. The weather has also played a critical role; recent unpredictable patterns have led to concerns about yield variability. Domestically, there's a strong competition from soybeans, with some farmers opting to switch due to better profitability and less intensive water requirements, impacting corn supply levels. Internationally, trade tensions continue to create uncertainty, influencing export markets and price stability." + "\n\n"
+  final_report = final_report_weather + final_report_soil + final_report_market_conditions + "\n\n"
+
   return final_report
 
 def main_custom_location(lat, lon):
-  # return main("https://api.agromonitoring.com/agro/1.0/weather/forecast?lat=-21.708915&lon=-44.016018&appid=a67805072a2d693ecebc6489037eedc5")
   return main(f"https://api.agromonitoring.com/agro/1.0/weather/forecast?lat={lat}&lon={lon}&appid={os.getenv('api_key')}")
 
-if st.button('Get Weather Report'):
-  if (option == "Brazil"):
+if st.button('Get Report'):
+  if (option == "Taquara Farm in Rio de Janeiro - Brazil", "Susan's Sweet Potatoes Farm - Iowa"):
     st.write(main(brazil))
-  elif (option == "Iowa"):
+  elif (option == "Susan's Sweet Potatoes Farm - Iowa"):
     st.write(main(iowa))
   elif (option == "Custom"):
     st.write(main_custom_location(custom_lat, custom_long))
